2669352bdc3be47c7f70744bbfd2fd534ba0492c
Gaurav Jain <gauravj49@gmail.com>
2019-08-30 09:24:25 +0200

    Modified wrapper to make it more streamlined with variables and removed redundant code

--- 01_mapping_usage.sh
+++ 01_mapping_usage.sh
@@ -1,12 +1,16 @@
+# pwd
+cd /home/rad/users/gaurav/projects/seqAnalysis/atacseq
 
-# Run in parallel
-cmd="parallel ::: "
-for s in scripts/01_map_fastQ_bowtie2/AGRad_ATACseq_MUC001/*.sh
-do 
-	chmod 775 ${s}
-	cmd=$(echo "${cmd} ${s}")
-done
+############### For Users ###################
+# Get the wrappers and then run them in parallel
 
-echo ${cmd}
-# to run
-eval ${cmd}
+# 1) Christine Klement
+# 1.1) For the first run for ATACseq_MUC001 sampels
+species="mm10"
+fastqdir="/home/rad/users/gaurav/projects/misc/output/AGRad_ATACseq_MUC001"
+outputdir="output/AGRad_ATACseq_MUC001/mapping"
+scriptsdir="${jobdir}/scripts/01_map_fastQ_bowtie2/$(basename ${fastqdir})"
+
+bash 01_map_singleendFastQ_bowtie2.sh ${fastqdir} ${outputdir} ${species}
+cmd="parallel ::: "; for s in ${scriptsdir}/*.sh; do chmod 775 ${s}; cmd=$(echo "${cmd} ${s}"); done; eval ${cmd}
+
