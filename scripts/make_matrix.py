bb48f8923eb4e886f2cf323fa0c231a8bd11c869
Gaurav Jain <gauravj49@gmail.com>
2019-09-13 12:42:38 +0200

    Added the source and original author information

---
+++ scripts/make_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python env
+# Source: curl -O https://s3.amazonaws.com/bedtools-tutorials/web/make-matrix.py
+# http://quinlanlab.org/tutorials/bedtools/bedtools.html
+
+import sys
+import collections 
+
+matrix = collections.defaultdict(dict)
+for line in sys.stdin:
+	fields = line.strip().split()
+	matrix[fields[0]][fields[1]] = float(fields[2])
+
+keys = sorted(matrix.keys())
+sys.stdout.write("\t" + "\t".join(keys) + '\n')
+for k in keys:
+	sys.stdout.write(k)
+	for j in keys:
+		sys.stdout.write('\t' + str(matrix[k][j]))
+		sys.stdout.write('\n')
