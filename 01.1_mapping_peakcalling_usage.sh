4da85a7d0f733feba855bd6d16b51670202d39c8
Gaurav Jain <gauravj49@gmail.com>
2020-01-12 00:12:27 +0100

    ran QC for Snail001 samples

--- 01.1_mapping_peakcalling_usage.sh
+++ 01.1_mapping_peakcalling_usage.sh
@@ -145,22 +145,24 @@ multiqc -o ${multiqcDir} -n ${projName}_stats ${projDir}
 
 
 # 3) Christine Klement
-# 3.1) For the first run for ATACseq_MUC001 sampels
+# 3.1) For the first run for AGSaur_Snail001 sampels
+jobdir=" /home/rad/users/gaurav/projects/seqAnalysis/atacseq"
 species="mm10"
 user="christine"
-projName="AGRad_ATACseq_MUC001"
-fastqdir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001/demultiplexing/output/fastq"
-outputDir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001"
+projName="AGSaur_Snail001"
+fastqdir="/media/rad/HDD1/atacseq/christine/AGSaur_Snail001/fastq"
+outputDir="/media/rad/HDD1/atacseq/christine/AGSaur_Snail001"
+# outputDir="/media/rad/SSD1/atac_temp/christine/AGSaur_Snail001"
 scriptsdir="${jobdir}/scripts/01_map_fastQ_bowtie2/${projName}"
 multiqcDir="${outputDir}/qc/multiqc"; mkdir -p ${multiqcDir}
-fastqcDir="${outputDir}/qc/fastqc"; mkdir -p ${fastqcDir}
+fastqcDir="${outputDir}/qc/fastqc"; mkdir -p ${fastqcDir}/00_original
 logsDir="${outputDir}/logs"; mkdir -p ${logsDir}
 
 bash scripts/01_map_singleendFastQ_bowtie2.sh ${fastqdir} ${outputDir} ${projName} ${species}
 cmd="parallel ::: "; for s in ${scriptsdir}/*.sh; do chmod 775 ${s}; cmd=$(echo "${cmd} ${s}"); done; eval ${cmd}
 
 # Run multiqc on original files
-ls ${fastqdir}/*.gz | parallel --progress --eta -j 64 'fastqc -o ${fastqcDir}/00_original {}'
+ls ${fastqdir}/*.gz | parallel --progress --eta -j 64 "fastqc -o ${fastqcDir}/00_original {}"
 multiqc -o ${multiqcDir} -n 01_original_fastq_stats_${projName} ${fastqcDir}/00_original
 
 # Run multiqc on trimmed files
@@ -168,20 +170,20 @@ multiqc -o ${multiqcDir} -n 02_trimmed_fastq_stats_${projName} ${fastqcDir}/01_t
 
 # Get mapping stats and run multiqc on mapping logs
 bamdir="${outputDir}/bams/trimmed"
-ls ${bamdir}/*.bam | parallel --progress --eta -j 16 'samtools stats -@ 32 {} > {.}_mapping.logs'
+ls ${bamdir}/*.bam | parallel --progress --eta -j 16 "samtools stats -@ 32 {} > {.}_mapping.logs"
 mappingLogsDir="${outputDir}/logs/mappingLogs"; mkdir -p ${mappingLogsDir}; mv ${bamdir}/*.logs ${mappingLogsDir}
 multiqc -o ${multiqcDir} -n 03_mapping_stats_${projName} ${mappingLogsDir}
 
-# Get peaks and homer annotation
-jobdir=" /home/rad/users/gaurav/projects/seqAnalysis/atacseq"
-species="mm10"
-user="christine"
-projName="AGRad_ATACseq_MUC001"
-fastqdir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001/demultiplexing/output/fastq"
-outputDir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001"
-bamdir="${outputDir}/bams/trimmed"
-peaksdir="${outputDir}/peaks"
-scriptsdir="${jobdir}/scripts/02_peakCallingMACs_annotationHomer/${projName}"
-mkdir -p ${scriptsdir}
-for b in ${bamdir}/*.bam; do bash scripts/02_peakCallingMACS2_annotationHomer.sh ${b} ${peaksdir} ${species} ${projName}; done;
-cmd="parallel --tmpdir /media/rad/SSD1/atac_temp ::: "; for s in ${scriptsdir}/*.sh; do chmod 775 ${s}; cmd=$(echo "${cmd} ${s}"); done; eval ${cmd}
+# # Get peaks and homer annotation
+# jobdir=" /home/rad/users/gaurav/projects/seqAnalysis/atacseq"
+# species="mm10"
+# user="christine"
+# projName="AGRad_ATACseq_MUC001"
+# fastqdir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001/demultiplexing/output/fastq"
+# outputDir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001"
+# bamdir="${outputDir}/bams/trimmed"
+# peaksdir="${outputDir}/peaks"
+# scriptsdir="${jobdir}/scripts/02_peakCallingMACs_annotationHomer/${projName}"
+# mkdir -p ${scriptsdir}
+# for b in ${bamdir}/*.bam; do bash scripts/02_peakCallingMACS2_annotationHomer.sh ${b} ${peaksdir} ${species} ${projName}; done;
+# cmd="parallel --tmpdir /media/rad/SSD1/atac_temp ::: "; for s in ${scriptsdir}/*.sh; do chmod 775 ${s}; cmd=$(echo "${cmd} ${s}"); done; eval ${cmd}
