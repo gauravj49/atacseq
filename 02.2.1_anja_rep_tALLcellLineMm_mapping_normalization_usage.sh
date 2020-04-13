20bf2bbf5bbef08da6218881b15b1c0903b49dac
Gaurav Jain <gauravj49@gmail.com>
2020-04-13 23:00:04 +0200

    updated analysis for the replicates peaks

--- 02.2.1_anja_rep_tALLcellLineMm_mapping_normalization_usage.sh
+++ 02.2.1_anja_rep_tALLcellLineMm_mapping_normalization_usage.sh
@@ -23,30 +23,33 @@ suppressPackageStartupMessages(library("DESeq2", warn.conflicts=FALSE, quietly=T
 suppressPackageStartupMessages(library("data.table", warn.conflicts=FALSE, quietly=TRUE))
 suppressPackageStartupMessages(library("splitstackshape", warn.conflicts=FALSE, quietly=TRUE))
 
-inputFile  <- "/media/rad/HDD1/atacseq/anja/tALLcellLineMm/analysis/mergedReps/tALLcellLineMm_mergedreps_all_merge_master_peaks_rawCounts.matrix"
-outputFile <- "/media/rad/HDD1/atacseq/anja/tALLcellLineMm/analysis/mergedReps/tALLcellLineMm_mergedreps_all_merge_master_peaks_vstCounts.matrix"
+inputFile  <- "/media/rad/HDD1/atacseq/anja/rep_tALLcellLineMm/analysis/rep_tALLcellLineMm_analysis_consensus_peaks_rawCounts.txt"
+outputFile <- "/media/rad/HDD1/atacseq/anja/rep_tALLcellLineMm/analysis/rep_tALLcellLineMm_analysis_consensus_peaks_vstCounts.txt"
 
 # Import data from featureCounts
-countdata  <- read.table(inputFile, header=TRUE, row.names=1, check.names = FALSE)
+countdata  <- fread(inputFile, header=TRUE)
+
+# Subset genomic annoations from the counts DT
+annotDT <- countdata[, c('PeakChrom','PeakStart','PeakEnd','PeakID')]
+
+# Remove PeakChrom PeakStart  PeakEnd columns
+countdata[, c('PeakChrom','PeakStart','PeakEnd','PeakID'):=NULL]  
 
 # Create the sampletable from the column names
 targetsDT <- data.table(colnames(countdata))
 SampleTable <- cSplit(targetsDT, 'V1','_', drop=F)
 
 # Create new column batch column
-batches    <- c(1,1,2,2,1,1,2,2,3,3,2,2,2,1)
+batches    <- c(1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,1,1,2,2,1,1,1,1,1,1)
 SampleTable$batches <- batches
 
-# Rename V_1 column to sampleNames
-names(SampleTable)[names(SampleTable) == "V1"]   <- "sampleNames"
+# Rename V1_1 column to sampleNames
+names(SampleTable)[names(SampleTable) == "V1_1"]   <- "sampleNames"
 
 # Drop useless columns
-SampleTable <- subset(SampleTable, select = -c(V1_1, V1_2, V1_3, V1_4, V1_5, V1_6, V1_7, V1_8, V1_9))
+SampleTable <- subset(SampleTable, select = -c(V1_2, V1_3, V1_4, V1_5, V1_6))
 
 # Get the dds object
-# coldata   <- data.frame(row.names=colnames(countdata), samples=colnames(countdata))
-# dds       <- DESeqDataSetFromMatrix(countData=countdata, design=~1, colData=coldata)
-
 # the design formula contains a numeric variable with integer values,
 # specifying a model with increasing fold change for higher values.
 # did you mean for this to be a factor? if so, first convert
@@ -55,13 +58,15 @@ SampleTable$batches <- as.factor(SampleTable$batches)
 dds <- DESeqDataSetFromMatrix(colData  = SampleTable, countData=countdata, design = ~batches)
 
 # VST 
-vst         <- varianceStabilizingTransformation(dds, blind=TRUE)  
-vsd         <- assay(vst)
+vst      <- varianceStabilizingTransformation(dds, blind=TRUE)  
+vsd      <- assay(vst)
 
-# Convert rownames as feature column to save it in the csv file
-vsd           <- cbind(feature = rownames(vsd), vsd)
-rownames(vsd) <- 1:nrow(vsd)
+# Add the genomic annotation in base R
+vstBindDT <- cbind(annotDT, vsd)
+
+# Round all normalized counts to 4 decimal places
+vstOutDT  <- data.table(vstBindDT %>% mutate_at(vars(-PeakChrom,-PeakStart,-PeakEnd,-PeakID), funs(round(., 4))))
 
 # Save the counts file to output csv file
-write.table(vsd   , file = outputFile , row.names = F, sep = '\t', quote = F)
+fwrite(vstOutDT   , file = outputFile , sep = '\t', quote = F)
 
