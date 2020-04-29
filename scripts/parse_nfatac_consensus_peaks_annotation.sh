672b301c33b051ff34dd2fda5f9e7a5c8cb37061
Gaurav Jain <gauravj49@gmail.com>
2020-04-29 04:04:28 +0200

    Generate consensus peaks raw counts and annotation

--- scripts/parse_nfatac_consensus_peaks_annotation.sh
+++ scripts/parse_nfatac_consensus_peaks_annotation.sh
@@ -6,65 +6,53 @@ species=${1:-"mouse"}
 user=${2:-"anja"}
 projName=${3:-"rep_tALLcellLineMm"}
 outdir=${4:-"/media/rad/HDD1/atacseq"}
-jobdir=${5:-"/home/rad/users/gaurav/projects/seqAnalysis/atacseq"}
+origConsFile=${5:-""} # /media/rad/HDD1/atacseq/anja/nfTALLMm/results/bwa/mergedReplicate/macs/broadPeak/consensus/deseq2/consensus_peaks.mRp.clN.featureCounts.txt
+origAnnFile=${6:-""}  # /media/rad/HDD1/atacseq/anja/nfTALLMm/results/bwa/mergedReplicate/macs/broadPeak/consensus/consensus_peaks.mRp.clN.boolean.annotatePeaks.txt
+jobdir=${7:-"/home/rad/users/gaurav/projects/seqAnalysis/atacseq"}
+
+############################
+# Remove later
+species="mouse"
+user="anja"
+projName="nfTALLMm"
+outdir="/media/rad/HDD1/atacseq"
+origConsFile="/media/rad/HDD1/atacseq/anja/nfTALLMm/results/bwa/mergedReplicate/macs/broadPeak/consensus/deseq2/consensus_peaks.mRp.clN.featureCounts.txt"
+origAnnFile="/media/rad/HDD1/atacseq/anja/nfTALLMm/results/bwa/mergedReplicate/macs/broadPeak/consensus/consensus_peaks.mRp.clN.boolean.txt"
+jobdir="/home/rad/users/gaurav/projects/seqAnalysis/atacseq"
+############################
 
 # Get relevant directories
 projDir="${outdir}/${user}/${projName}"
-peaksdir="${projDir}/peaks"
 analysisDir="${projDir}/analysis"; mkdir -p ${analysisDir}
-peakFilesDir="${analysisDir}/broadPeaks"; mkdir -p ${peakFilesDir}
 
-# Link broad peaks into a separate directory
-ln -s ${peaksdir}/*/macs2peaks/*.broadPeak ${peakFilesDir}
-
-# Get peaks summit for broad peaks
-for broadpeak in ${peakFilesDir}/*.broadPeak; 
-do 
- bname=$(basename ${broadpeak} _peaks.broadPeak); 
- echo ${bname}; 
- bamfile=${projDir}/bams/trimmed/${bname}.bam; 
- summitfile=${peaksdir}/${bname}/macs2peaks/${bname}_peaks.summit
- macs2 refinepeak -b ${broadpeak} -i ${bamfile} -o ${summitfile}
- echo ""
-done
-
-# Link broad peaks into a separate directory
-ln -s ${peaksdir}/*/macs2peaks/*.summit ${peakFilesDir}
-
-# Get the peak filenames in a file
-ls ${peakFilesDir}/* > ${analysisDir}/peaksFileList.txt
-
-# Get consensus peaks (Atacseq peaks are broad peaks)
-echo "- Getting consensus peaks (Atacseq peaks are broad peaks)"
+# Get parsed consensus peaks bed (Atacseq peaks are broad peaks)
+echo "- Getting parsed consensus peaks bed (Atacseq peaks are broad peaks)"
+origConsBed="$(dirname ${origAnnFile})/$(basename ${origAnnFile} .boolean.annotatePeaks.txt).bed"
 consensusPeaksBed="${analysisDir}/${projName}_$(basename ${analysisDir})_consensus_peaks.bed"
-cat ${peakFilesDir}/*.broadPeak | sort -k1,1V -k2,2g > ${consensusPeaksBed}.tmp
-mergeBed -i ${consensusPeaksBed}.tmp > ${consensusPeaksBed}
-rm ${consensusPeaksBed}.tmp
+awk 'BEGIN { OFS="\t" }{$3=$3 OFS $4"_"$1"_"$2"_"$3}1' ${origConsBed} | cut -f1-4,6,7 | sort -k1,1V -k2,2g > ${consensusPeaksBed}
 
-# Get the tab seaprated raw counts of the merged peaks for all the samples
-# Using deeptools
-rawCountsBname="${analysisDir}/$(basename ${consensusPeaksBed} .bed)_rawCounts"
-multiBamSummary BED-file --BED ${consensusPeaksBed} --bamfiles ${projDir}/bams/trimmed/*.bam --smartLabels -out ${rawCountsBname}.npz --outRawCounts ${rawCountsBname}.txt -p 64
+# Get the tab seaprated raw counts matrix for consensus peaks on all samples
+echo "- Getting the tab seaprated raw counts matrix for consensus peaks on all samples)"
+rawCountsTxtFile="${analysisDir}/$(basename ${consensusPeaksBed} .bed)_rawCounts.txt"
+sed 1d ${origConsFile} | awk 'BEGIN { OFS="\t" }{$4=$4 OFS $1"_"$2"_"$3"_"$4}1' | cut -f2-5,8- > ${rawCountsTxtFile}
 
 # Sort the input file with the header line intact
-rawCountsTxtFile="${rawCountsBname}.txt"
 (head -n 1 ${rawCountsTxtFile} && tail -n +2 ${rawCountsTxtFile} | sort -k1,1V -k2,2g -k3,3g) > ${rawCountsTxtFile}.tmp && mv ${rawCountsTxtFile}.tmp ${rawCountsTxtFile}
 
-# Remove single quotes from the header
-sed -i "s/[']//g" ${rawCountsTxtFile}
+# Remove additional characters from the header
+sed -i "s/.mLb.clN.bam//g" ${rawCountsTxtFile}
 
 # Rename columns. \b is word boundary to search for full word only
-sed -i "s/#chr\b/PeakChrom/" ${rawCountsTxtFile}
-sed -i "s/start\b/PeakStart/" ${rawCountsTxtFile}
-sed -i "s/end\b/PeakEnd/" ${rawCountsTxtFile}
+sed -i "s/Chr\b/PeakChrom/" ${rawCountsTxtFile}
+sed -i "s/Start\b/PeakStart/" ${rawCountsTxtFile}
+sed -i "s/End\b/PeakEnd/" ${rawCountsTxtFile}
+sed -i "s/Geneid_Chr_Start_End\b/PeakID/" ${rawCountsTxtFile}
 
-# Change float to integer in the python one liner
-python -c "import sys; import pandas as pd; import datatable as dt; input_file=sys.argv[1]; outtxt_file=sys.argv[2]; peaksDT = dt.fread(input_file, sep='\t', header=True, nthreads=16);peaksDF = peaksDT.to_pandas(); peaksDF.to_csv(outtxt_file, header=True, index=False, sep='\t', float_format='%.0f')" ${rawCountsTxtFile} ${rawCountsTxtFile}.tmp
+# Get parsed boolean annotation file
+peaksAnnTabFile="${analysisDir}/$(basename ${consensusPeaksBed} .bed)_annotation.tab"
+Rscript -e 'library(data.table); inputfile  <- commandArgs(TRUE)[1]; outputfile <- commandArgs(TRUE)[2]; peaksDT    <- fread(inputfile, header=TRUE, sep="\t"); dropCols   <- grep("(fc|qval|pval)$", colnames(peaksDT)); peaksDT[,(dropCols) :=NULL]; (to.replace <- names(which(sapply(peaksDT, is.logical)))); for (var in to.replace) peaksDT[, (var):= as.numeric(get(var))]; 
+peaksDT[,PeakID:=paste(interval_id,chr,start,end, sep="_")]; setnames(peaksDT, c("PeakChrom", "PeakStart", "PeakEnd","PeakID","NumPeaks","NumSamples"));fwrite(peaksDT, outputfile, sep = "\t");' ${origAnnFile} ${peaksAnnTabFile}
 
-# Add PeakID column
-# Source for concatenating: https://unix.stackexchange.com/questions/304499/adding-column-to-a-table-by-concatenating-values-from-other-columns
-# source for line numbers: https://stackoverflow.com/questions/20752043/print-line-numbers-starting-at-zero-using-awk
-awk 'BEGIN { OFS="\t" }{$3=$3 OFS "atacPeak_"i++"_"$1"_"$2"_"$3}1' ${rawCountsTxtFile}.tmp | awk 'BEGIN { OFS="\t" }NR==1{$4="PeakID"}1' > ${rawCountsTxtFile} && rm ${rawCountsTxtFile}.tmp
 
 ############# Annotation Matrix File #############
 # Create binary peaks flag annotation matrix
@@ -125,5 +113,7 @@ echo "Rscript ${jobdir}/scripts/R_annotate_peaks_chipseeker.R -if=${peaksAnnTabF
 echo "-------------------------------------------------------------------"
 Rscript ${jobdir}/scripts/R_annotate_peaks_chipseeker.R -if=${peaksAnnTabFile} -of=${peaksAnnTxtFile} -sp=${species} 2>&1 | tee -a ${annotationLogFile}
 
+# Remove the peaksAnnTabFile file as the data is there in the peaksAnnTxtFile
+rm ${peaksAnnTabFile}
 
 
